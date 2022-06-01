afda98ccedbba8f09f7111e26ea0fb06843e12fd
Alvaro-SP <socop2412@gmail.com>
2022-06-01 14:45:16 -0600

    commit.

---
+++ Commit automatico.bat
@@ -0,0 +1,6 @@
+
+cd "C:\Users\spuser\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\5.1 VAQUERAS JUNIO 2022\LAB COMPILADORES\Java-and-ANTLR-tool-Parsing-COMPI2"
+git add .
+git commit -m "commit."
+git push
+pause
