7ac6067a611282875b3dbf4ef13d2af3bba90127
Alvaro-SP <socop2412@gmail.com>
2022-06-02 22:07:34 -0600

    commit.

--- Commit automatico.bat
+++ Commit automatico.bat
@@ -1,5 +1,7 @@
 
-cd "C:\Users\spuser\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\5.1 VAQUERAS JUNIO 2022\LAB COMPILADORES\Java-and-ANTLR-tool-Parsing-COMPI2"
+@REM cd "C:\Users\spuser\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\5.1 VAQUERAS JUNIO 2022\LAB COMPILADORES\Java-and-ANTLR-tool-Parsing-COMPI2"
+@echo off
+echo %~dp0
 git add .
 git commit -m "commit."
 git push
